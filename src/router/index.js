6dd51eb030597b3ef22b962690cbcf6912635e4a
易川阳 <yichuanyang@cdyhsoft.com>
2018-09-20 18:23:25 +0800

    commit this in new code from my volunteer

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld';
+import MainLogin from '@/pages/login';
+import RecordEntry from '@/pages/recordEntry';
 
 Vue.use(Router)
 
@@ -8,8 +10,26 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'MainLogin',
+      component: MainLogin,
+      meta: {
+        title: '登录志愿填报'
+      },
+      children:[{
+        path: '/login',
+        name: 'MainLogin',
+        component: MainLogin,
+        meta: {
+          title: '登录志愿填报'
+        }
+      },{
+        path: '/recordEntry',
+        name: 'recordEntry',
+        component: RecordEntry,
+        meta: {
+          title: '录入成绩'
+        }
+      },]
     }
   ]
 })
